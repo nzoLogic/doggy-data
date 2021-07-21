dba33d15e258e72a58b9d3e9e39abf8a06b91145
Aaron Steele <nzoLogic@gmail.com>
2021-07-21 15:19:06 -0400

    add typography to theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { ThemeProvider } from '@material-ui/styles';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { ReactComponent as Logo } from './assets/doggy-data-logo.svg';
 import AppRouter from './AppRouter';
 import theme from './theme.js';
 
+const customTheme = createTheme({
+  typography: {
+    fontFamily: ['Lato', 'san-serif'].join(',')
+  },
+})
 function App() {
   return (
     <div>
-      <ThemeProvider theme={ theme }>
+      <ThemeProvider theme={ {
+        ...theme,
+        ...customTheme,
+      }
+      } >
+        <CssBaseline />
         <Logo />
         <AppRouter />
       </ThemeProvider>
