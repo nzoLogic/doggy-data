da378dbb9a7faa787fcefc67f396a4449b66f52d
Aaron Steele <nzoLogic@gmail.com>
2021-07-21 10:15:48 -0400

    switch render and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App()
 {
   return (
     <div>
-      hi
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ () => <div>DoggyData1</div> } />
+          <Route path="/greeting" component={ () => <div>DoggyData2</div> } />
+          <Route path="/logging-info-1" component={ () => <div>DoggyData3</div> } />
+          <Route path="/logging-info-2" component={ () => <div>DoggyData4</div> } />
+          <Route path="/profile" component={ () => <div>DoggyData5</div> } />
+        </Switch>
+      </Router>
     </div>
   );
 }
